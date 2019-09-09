91bd6261788e70dc5bf00cc98924770b2a6a917a
jen <fake@mail>
2019-09-10 01:27:07 +0300

    Added PageManager. Edited gitignore.

---
+++ ImageCompareMap/Model/PageManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mapcompare.Model
+{
+    static class PageManager
+    {
+        public static void SetCurrentPage ()
+        {
+
+        }
+    }
+}
